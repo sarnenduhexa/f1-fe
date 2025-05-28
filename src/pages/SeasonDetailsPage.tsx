c90a1aac2038b01603bb91239df6e95e7ae7074f
Sarnendu Mukherjee <sarnendu.hexa@gmail.com>
2025-05-28 13:39:37 +0200

    feat: try to fetch the winner driver details again if season in context does not have winner data.

--- src/pages/SeasonDetailsPage.tsx
+++ src/pages/SeasonDetailsPage.tsx
@@ -17,13 +17,14 @@ const SeasonDetailsPage: React.FC = () => {
   const navigate = useNavigate();
 
   // Fetch season if not in context (deep link/refresh)
+  // or if the season has no winner driver id
   const {
     data: fetchedSeason,
     isLoading: isSeasonLoading,
     isError: isSeasonError,
     error: seasonError,
   } = useSeasonsControllerFindOne(Number(seasonId!), {
-    query: { enabled: !season },
+    query: { enabled: !season || !season.winnerDriverId },
   });
 
   useEffect(() => {
